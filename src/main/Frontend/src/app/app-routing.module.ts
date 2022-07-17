6ab167d5130c7c1e1e1462112f43124aae30c5fd
HariharGandhi <gandhiharihar001@gmail.com>
2022-07-17 09:38:28 +0530

    Login_Regiter_Page_code updation

--- src/main/Frontend/src/app/app-routing.module.ts
+++ src/main/Frontend/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { StudentComponent } from './student/student.component';
 
-const routes: Routes = [];
+const routes: Routes = [{path:"Sign-in",component:LoginComponent},
+                        {path:"Register",component:RegisterComponent},
+                        {path:"Login",component:StudentComponent},
+                        {path:"home",component:HomeComponent},
+                        {path:"",redirectTo:"",pathMatch:"full"}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/main/Frontend/src/app/app.module.ts
+++ src/main/Frontend/src/app/app.module.ts
@@ -3,14 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from './home/home.component';
+import { StudentComponent } from './student/student.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent,
+    StudentComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
